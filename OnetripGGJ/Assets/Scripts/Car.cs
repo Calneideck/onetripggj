80cc2adb7c55ab387f88a87238460f1de61b6009
Callum Neideck <cal_neideck@hotmail.com>
2016-01-29 21:28:39 +1100

    Created Car

---
+++ OnetripGGJ/Assets/Scripts/Car.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour
+{
+    public float speed;
+    public float maxSpeed;
+    public float rotationSpeed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(transform.right * -1, 1 * Time.deltaTime);
+        }
+    }
+
+	void FixedUpdate ()
+    {
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(transform.forward * speed);
+
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(transform.forward * -1 * speed);
+
+        if (rb.velocity.magnitude > maxSpeed)
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+    }
+}
