3eae8adc0b0be18f80273edf455679d2b47d8a18
unknown <GGJAM2016@ATC325-12.ds.swin.edu.au>
2016-01-29 22:11:22 +1100

    Added Credits

---
+++ OnetripGGJ/Assets/Scripts/Blogs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blogs : MonoBehaviour {
+	public void Dimitri() {
+		Application.OpenURL("http://sarkatzis.tumblr.com/");
+	}
+	public void Jonathan() {
+		Application.OpenURL("http://jbrymcfly.tumblr.com/");
+	}
+}
