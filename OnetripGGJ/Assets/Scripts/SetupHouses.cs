ab44effeff7bbc4f7b19294e0996cb5062e08404
Callum Neideck <cal_neideck@hotmail.com>
2016-01-30 18:26:41 +1100

    Starting to fix package type heights

--- OnetripGGJ/Assets/Scripts/SetupHouses.cs
+++ OnetripGGJ/Assets/Scripts/SetupHouses.cs
@@ -5,14 +5,17 @@ using System.Collections.Generic;
 
 public class SetupHouses : MonoBehaviour
 {
-    public Transform[] spawnLocations;
-    public Transform[] packageTypeSpawnLocations;
     public GameObject[] housePrefabs;
-    public GameObject[] packageTypePrefabs;
-    public Material[] wallMaterials;
+    public Transform[] houseSpawnLocations;
     public Transform houseHolderObject;
+    public Material[] wallMaterials;
+
+    public GameObject[] packageTypePrefabs;
+    public Transform[] packageTypeSpawnLocations;
+    public Transform packageTypeHolderObject;
 
     private List<HouseStruct> houses = new List<HouseStruct>();
+    private float[] heightOffsets = new float[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, };
 
     void Start ()
     {
@@ -30,14 +33,15 @@ public class SetupHouses : MonoBehaviour
         }
 
         // Create the actual house objects and assign their info using the above order
-        for (int i = 0; i < spawnLocations.Length; i++)
+        for (int i = 0; i < houseSpawnLocations.Length; i++)
         {
             HouseStruct houseInfo = houses[i];
-            GameObject house = (GameObject)GameObject.Instantiate(housePrefabs[Random.Range(0, housePrefabs.Length)], spawnLocations[i].position, Quaternion.identity);
+            GameObject house = (GameObject)GameObject.Instantiate(housePrefabs[Random.Range(0, housePrefabs.Length)], houseSpawnLocations[i].position, Quaternion.identity);
             house.transform.SetParent(houseHolderObject);
             house.GetComponent<House>().Setup(wallMaterials[(int)houseInfo.colour], houseInfo.colour, packageTypePrefabs[(int)houseInfo.packageType], houseInfo.packageType);
 
-            GameObject.Instantiate(packageTypePrefabs[(int)houseInfo.packageType], packageTypeSpawnLocations[i].position, Quaternion.identity);
+            // Create the package type object
+            GameObject.Instantiate(packageTypePrefabs[(int)houseInfo.packageType], packageTypeSpawnLocations[i].position + Vector3.up * heightOffsets[(int)houseInfo.packageType], Quaternion.identity);
         }
 	}
 }
