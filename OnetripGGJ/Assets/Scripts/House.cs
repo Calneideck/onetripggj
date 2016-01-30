17cb23346ddbfe70414ccec17876e167c7c0fb28
Callum Neideck <cal_neideck@hotmail.com>
2016-01-30 17:36:25 +1100

    Initial support for package type modifiers and camera smooth follow.

--- OnetripGGJ/Assets/Scripts/House.cs
+++ OnetripGGJ/Assets/Scripts/House.cs
@@ -3,14 +3,18 @@
 public class House : MonoBehaviour
 {
     public MeshRenderer[] renderers;
+    public Transform packageTypeSpawn;
 
     private HouseStruct houseInfo;
 
-    public void Setup(Material wallMaterial, HouseColour colour, PackageType packageType)
+    // Sets up the house assiging its colour to its renderer and creating the object that is used to uniquely identify it
+    public void Setup(Material wallMaterial, HouseColour colour, GameObject packageTypePrefab, PackageType packageType)
     {
         houseInfo = new HouseStruct(colour, packageType);
         foreach (MeshRenderer renderer in renderers)
             renderer.material = wallMaterial;
+
+        GameObject.Instantiate(packageTypePrefab, packageTypeSpawn.position, Quaternion.identity);
     }
 
     public HouseStruct HouseInfo
